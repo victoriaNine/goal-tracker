dc075bee466ec859238b6115d2be68817a274a27
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 12:30:15 +0100

    Début écriture LoginScreen

--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -4,3 +4,15 @@ import ArrowForward from 'material-ui/svg-icons/navigation/arrow-forward'
 import { Card, CardActions, CardText, CardTitle } from 'material-ui/Card'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
+
+import '../styles/LoginScreen.styl'
+
+export class LoginScreen extends Component {
+  render () {
+    return (
+      <p>COMING SOON: LOGIN SCREEN</p>
+    )
+  }
+}
+
+export default LoginScreen
