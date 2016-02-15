ce4fb0abc46f29debacf6dd6de31f73d1294012e
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 14:55:41 +0100

    Début étape 11 : plomberie du routage

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { Provider } from 'react-redux'
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory as history } from 'react-router'
 
 // DEV NOTE: only required until Material UI 0.16+, as it won't use
 // `onTouchTap` unduly anymore.
 injectTapEventPlugin()
 
+import App from './components/App'
+import HistoryScreen from './containers/HistoryScreen'
 import HomeScreen from './containers/HomeScreen'
+import SettingsScreen from './containers/SettingsScreen'
 import store from './store'
+
+function requireAuth (nextState, replace) {
+  if (store.getState().currentUser.loginState !== 'success') {
+    replace('/')
+  }
+}
 
 render(
   <Provider store={store}>
