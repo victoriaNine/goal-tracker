9d0ede7f8565f02937a7a4eca21ba5aad333a733
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 14:36:26 +0100

    Début étape 4 : bases pour Action Creators, Reducer et tests "Current User"

---
+++ src/reducers/currentUser.spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+
+import { logIn, logOut } from '../action-creators'
+import reducer from './currentUser'
+
+describe('Current User reducer', () => {
+  it('should return its initial state')
+
+  it('should handle login')
+
+  it('should handle logout')
+})
