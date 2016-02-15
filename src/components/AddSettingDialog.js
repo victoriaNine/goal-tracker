03c8c892c1d38038aa061474e099c2ec01b09519
Christophe Porteneuve <tdd@tddsworld.com>
2016-02-15 15:19:40 +0100

    Début étape 14 : SettingsScreen

---
+++ src/components/AddSettingDialog.js
@@ -0,0 +1,90 @@
+import autobind from 'autobind-decorator'
+import React, { Component, PropTypes } from 'react'
+
+import Checkbox from 'material-ui/Checkbox'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import ContentEdit from 'material-ui/svg-icons/content/create'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import TextField from 'material-ui/TextField'
+
+import { GoalPropType } from '../prop-types'
+
+const DEFAULT_STATE = { id: undefined, name: '', target: 5, units: '', keepOpen: true }
+
+export default class AddSettingDialog extends Component {
+  static propTypes = {
+    goal: PropTypes.oneOfType([GoalPropType, PropTypes.shape({})]),
+    onAdd: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired
+  }
+
+  constructor (...args) {
+    super(...args)
+    this.state = DEFAULT_STATE
+  }
+
+  componentWillReceiveProps (newProps) {
+    // ???
+  }
+
+  handleChange (event, field, checked) {
+    if (field === 'keepOpen') {
+      this.setState({ keepOpen: checked })
+    } else {
+      const caster = field === 'target' ? Number : String
+      this.setState({ [field]: caster(event.target.value) })
+    }
+  }
+
+  render () {
+    const { open, onCancel } = this.props
+    const isEditing = 'id' in this.props.goal
+
+    const actions = [
+      <FlatButton label='Annuler' secondary onTouchTap={onCancel} />
+    ]
+    if (isEditing) {
+      actions.push(<FlatButton label='Modifier' primary onTouchTap={this.triggerAdd} icon={<ContentEdit />} />)
+    } else {
+      actions.push(<FlatButton label='Ajouter' primary onTouchTap={this.triggerAdd} icon={<ContentAdd />} />)
+    }
+
+    return (
+      <Dialog
+        title={isEditing ? 'Modifier un objectif' : 'Ajouter un objectif'}
+        actions={actions}
+        open={open}
+        onRequestClose={onCancel}
+      >
+        <div>
+          <TextField name='name' floatingLabelText='Nom' required fullWidth autoFocus
+            value={this.state.name} onChange={(event) => this.handleChange(event, 'name')}
+          />
+          <TextField name='target' floatingLabelText='Quantité par jour' type='number' required
+            value={this.state.target} onChange={(event) => this.handleChange(event, 'target')}
+          />
+          {' '}
+          <TextField name='units' floatingLabelText='Unité' hintText='pas, minutes de course…' required
+            value={this.state.units} onChange={(event) => this.handleChange(event, 'units')}
+          />
+          {isEditing || (
+            <Checkbox
+              label='Garder ouvert pour l’ajout suivant'
+              checked={this.state.keepOpen}
+              onCheck={(event, checked) => this.handleChange(event, 'keepOpen', checked)}
+              style={{ marginTop: '1em' }}
+            />
+          )}
+        </div>
+      </Dialog>
+    )
+  }
+
+  @autobind
+  triggerAdd () {
+    this.props.onAdd(this.state)
+    this.setState(DEFAULT_STATE)
+  }
+}
--- src/containers/SettingsScreen.js
+++ src/containers/SettingsScreen.js
@@ -1,8 +1,7 @@
 import autobind from 'autobind-decorator'
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import DocumentTitle from 'react-document-title'
-import { Link } from 'react-router'
+import { browserHistory as history, Link } from 'react-router'
 import React, { Component } from 'react'
 
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
@@ -14,23 +13,65 @@ import IconButton from 'material-ui/IconButton'
 import { List, ListItem } from 'material-ui/List'
 import Logout from 'material-ui/svg-icons/action/exit-to-app'
 import RaisedButton from 'material-ui/RaisedButton'
+import Subheader from 'material-ui/Subheader'
 
-import * as actionCreators from '../action-creators'
+import { addGoal, logOut, removeGoal, updateGoal } from '../action-creators'
+import AddSettingDialog from '../components/AddSettingDialog'
+import DeleteSettingDialog from '../components/DeleteSettingDialog'
+import GoalSetting from '../components/GoalSetting'
 
 class SettingsScreen extends Component {
   render () {
+    const { currentUser, goals, logOut } = this.props
+    const logoutButton = (
+      <IconButton onClick={logOut}>
+        <Logout />
+      </IconButton>
+    )
+
     return (
       <DocumentTitle title='Mes paramètres'>
-        <h1>Settings coming soon</h1>
+        <div>
+          <FlatButton label='Retour'
+            icon={<ArrowBack />} containerElement={<Link to='/' />}
+          />
+          <Card className='settings'>
+            <CardTitle title='Paramètres' />
+            <CardText>
+              <List>
+                <ListItem
+                  primaryText='Vous êtes connecté-e en tant que'
+                  secondaryText={currentUser.email}
+                  rightIconButton={logoutButton}
+                />
+              </List>
+              <Divider />
+              <List>
+                <Subheader>Mes objectifs</Subheader>
+                {goals.map((goal) =>
+                  <GoalSetting key={goal.id} goal={goal}
+                  />
+                )}
+                {goals.length === 0 && (
+                  <ListItem secondaryText='Aucun objectif pour le moment' />
+                )}
+              </List>
+            </CardText>
+            <CardActions>
+              <RaisedButton label='Ajouter un objectif' primary
+                icon={<ContentAdd />}
+              />
+            </CardActions>
+          </Card>
+        </div>
       </DocumentTitle>
     )
   }
 }
 
 const mapStateToProps = ({ goals, currentUser }) => ({ goals, currentUser })
-const mapDispatchToProps = (dispatch) => {
-  const { addGoal, removeGoal, updateGoal, logOut } = actionCreators
-  return bindActionCreators({ addGoal, removeGoal, updateGoal, logOut }, dispatch)
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(SettingsScreen)
+export default connect(
+  mapStateToProps,
+  { addGoal, logOut, removeGoal, updateGoal }
+)(SettingsScreen)
