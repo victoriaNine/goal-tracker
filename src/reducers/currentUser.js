ce7c6d8bb014c7891cdca3ab0afb27919a206215
Christophe Porteneuve <tdd@tddsworld.com>
2016-05-02 11:46:22 +0200

    Étape 4 : Action Creators, Reducer et tests "Current User"

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,12 +1,12 @@
 import { LOGIN, LOGOUT } from '../action-creators'
 
-export default function currentUser (state = null, action) {
+export default function currentUser (state = { loginState: 'logged-out' }, action) {
   switch (action.type) {
     case LOGIN:
-      // Votre code ici
+      return { loginState: 'success', email: action.email }
 
     case LOGOUT:
-      // Votre code ici
+      return { loginState: 'logged-out' }
 
     default:
       return state
--- src/reducers/currentUser.spec.js
+++ src/reducers/currentUser.spec.js
@@ -4,9 +4,28 @@ import { logIn, logOut } from '../action-creators'
 import reducer from './currentUser'
 
 describe('Current User reducer', () => {
-  it('should return its initial state')
+  it('should return its initial state', () => {
+    const initialState = undefined
+    const expectedState = { loginState: 'logged-out' }
 
-  it('should handle login')
+    expect(reducer(initialState, {}))
+      .to.deep.equal(expectedState)
+  })
 
-  it('should handle logout')
+  it('should handle login', () => {
+    const email = 'john@example.com'
+    const initialState = { loginState: 'logged-out' }
+    const expectedState = { loginState: 'success', email }
+
+    expect(reducer(initialState, logIn(email, 'no fate')))
+      .to.deep.equal(expectedState)
+  })
+
+  it('should handle logout', () => {
+    const initialState = { loginState: 'success', email: 'john@example.com' }
+    const expectedState = { loginState: 'logged-out' }
+
+    expect(reducer(initialState, logOut()))
+      .to.deep.equal(expectedState)
+  })
 })
