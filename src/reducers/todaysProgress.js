906d47c557b3e3afa50442174d8cd61e33d7e61d
Christophe Porteneuve <tdd@tddsworld.com>
2017-01-21 16:37:57 +0100

    Étape 6 : reste des Action Creators et Reducers

--- src/reducers/todaysProgress.js
+++ src/reducers/todaysProgress.js
@@ -5,7 +5,7 @@ export default function todaysProgress (state = {}, action) {
     case PROGRESS:
       const previous = state[action.goalId] || 0
       const increment = Number(action.increment) || 0
-      return // ???
+      return { ...state, [action.goalId]: previous + increment }
 
     default:
       return state
